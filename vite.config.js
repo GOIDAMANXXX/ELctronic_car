be30a517211177640cc071db8ae8dd672d4524e4
GOIDAMANXXX <minjiailia@gmail.com>
2025-08-08 22:20:52 +0400

    Revert "update 5"
    This reverts commit bf31863381116c1b769d8187bdbeaee56dec4876.

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/ELctronic_car/',
-  plugins: [react()],
+  base: '/ELctronic_car/',  
+  plugins: [react()]
 })
