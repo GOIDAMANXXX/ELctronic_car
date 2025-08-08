8f1d9cef22d9bb5f14f778fb91dbd23a1779f2dc
GOIDAMANXXX <minjiailia@gmail.com>
2025-08-08 19:18:40 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/autoservice/',
+  base: '/ELctronic_car/',  
   plugins: [react()]
 })
